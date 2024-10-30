7fba2311b39f7c2e84a8e8a45ddf93f7dc42bebb
Kokanghee <kkh2261@gmail.com>
2024-10-30 14:25:49 +0900

    마이페이지 게시글 메인페이지에서 조회

--- Linklife2/linklife/src/main/java/com/mysite/sbb/MainController.java
+++ Linklife2/linklife/src/main/java/com/mysite/sbb/MainController.java
@@ -1,20 +1,37 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.post.Post;
+import com.mysite.sbb.post.PostService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.awt.print.Pageable;
+
 
 @Controller
 public class MainController {
 
-	@GetMapping("/sbb")
-	@ResponseBody
-	public String index() {
-		return "안녕하세요 sbb에 오신것을 환영합니다.";
+	private final PostService postService;
+
+	public MainController(PostService postService) {
+		this.postService = postService;
 	}
 
+	/*@GetMapping("/sbb")
+	@ResponseBody
+	public String index() {
+	return "안녕하세요 sbb에 오신것을 환영합니다.";
+	}
 	@GetMapping("/")
 	public String root() {
-		return "redirect:/user/login";
+	return "redirect:/user/login"; */
+
+	@GetMapping("/")
+	@ResponseBody
+	public Page<Post> getPosts(@RequestParam(defaultValue = "0") int page,
+							   @RequestParam(defaultValue = "") String keyword) {
+		return postService.getList(page, keyword);
 	}
 }
