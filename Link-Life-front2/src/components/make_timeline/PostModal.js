9be947969410aa1cb94b33ff7785cb37dadb28f0
admin <whyewon@naver.com>
2024-11-01 03:35:03 +0900

    Modal 꾸미기

--- Link-Life-front2/src/components/make_timeline/PostModal.js
+++ Link-Life-front2/src/components/make_timeline/PostModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
-import { FaEdit, FaRegImages } from "react-icons/fa";
+import { FaEdit, FaRegImages, FaMapMarkerAlt } from "react-icons/fa";
 
 const PostModal = ({ post, onClose }) => {
     const formatDate = (dateString) => {
@@ -10,6 +10,7 @@ const PostModal = ({ post, onClose }) => {
         const day = String(date.getDate()).padStart(2, '0');
         return `${year}년 ${month}월 ${day}일`; // 날짜 포맷
     };
+
     const [activeTab, setActiveTab] = useState(1);
 
     const renderContent = () => {
@@ -17,22 +18,27 @@ const PostModal = ({ post, onClose }) => {
             case 1:
                 return <p>{post.content || "행사 내용이 없습니다"}</p>;
             case 2:
-                return <p>{post.images || "사진이 없습니다"}</p>;
+                return (
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+                        <p>{post.eventLocation || "위치 정보가 없습니다"}</p>
+                    </div>
+                );
+            case 3:
+                return <p>{post.images || "사진 없음"}</p>;
             default:
                 return null;
         }
     };
 
-
     return (
         <Modal
             title={
-                <div style={{ marginTop: "10px", marginBottom: "10px" }}>
+                <div className={'post_title'} style={{ marginTop: "10px", marginBottom: "10px", backgroundColor: "#193623",
+                    color:"white", padding:"10px",borderRadius: "5px" }}>
                     {post.subject}
-                    {/* ---로 구분 */}
-                    <span style={{ margin: "20px" }}> ----- ♡ ----- </span>
+                    <span style={{ margin: "20px" }}> ---- ♡ ---- </span>
                     {formatDate(post.eventStartDateTime)}
-                </div> // 모달창 이름에 게시글 제목이랑 날짜 표시
+                </div>
             }
             onClose={onClose}
             width={600}
@@ -45,7 +51,7 @@ const PostModal = ({ post, onClose }) => {
                         onClick={() => setActiveTab(1)}
                     >
                         <div className="flex items-center justify-center text-sm font-semibold">
-                            <FaEdit className="w-5 h-5 mr-2"/>
+                            <FaEdit className="w-5 h-5 mr-2" />
                             행사
                         </div>
                     </div>
@@ -54,12 +60,21 @@ const PostModal = ({ post, onClose }) => {
                         onClick={() => setActiveTab(2)}
                     >
                         <div className="flex items-center justify-center text-sm font-semibold">
-                            <FaRegImages className="w-5 h-5 mr-2"/>
+                            <FaMapMarkerAlt className="w-5 h-5 mr-2" />
+                            위치
+                        </div>
+                    </div>
+                    <div
+                        className={activeTab === 3 ? "tab active-tab" : "tab"}
+                        onClick={() => setActiveTab(3)}
+                    >
+                        <div className="flex items-center justify-center text-sm font-semibold">
+                            <FaRegImages className="w-5 h-5 mr-2" />
                             사진
                         </div>
                     </div>
                 </div>
-                <div className="p-5">
+                <div className="post_content">
                     {renderContent()}
                 </div>
             </div>
